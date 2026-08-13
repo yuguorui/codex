yuguorui/codex-plus

codex-cli: add hashline tool
Add a new hashline tool that enables content-addressed line-level edits
using 2-character xxHash anchors. This provides a more robust alternative
to context-based patching by targeting exact line content rather than
surrounding text.

- New handlers/hashline.rs: implements read, edit, insert, delete, and
  verify actions with fuzzy relocate fallback for stale anchors
- New handlers/hashline_spec.rs: defines the tool schema with parameter
  validation for action, path, anchor, content, context, and before
- Fuzzy matching relocates to nearby lines when hash anchors don't match
  exactly, reducing false rejections from minor whitespace drift
- Generates file change events and diff output compatible with existing
  tool infrastructure
- Comprehensive test coverage for all actions and edge cases

codex-rs/core/src/tools/events.rs
@@ -141,6 +141,22 @@ impl ToolEmitter {
         }
     }
 
+    pub fn read(
+        command: Vec<String>,
+        cwd: AbsolutePathBuf,
+        source: ExecCommandSource,
+        name: String,
+        path: PathBuf,
+    ) -> Self {
+        let cmd = command.join(" ");
+        Self::Shell {
+            command,
+            cwd,
+            source,
+            parsed_cmd: vec![ParsedCommand::Read { cmd, name, path }],
+        }
+    }
+
     pub fn apply_patch(changes: HashMap<PathBuf, FileChange>, auto_approved: bool) -> Self {
         Self::ApplyPatch {
             changes,

yuguorui/codex

codex-rs/core/src/tools/events.rs
@@ -231,6 +231,23 @@ impl ToolEmitter {
         }
     }
 
+    pub fn read(
+        command: Vec<String>,
+        cwd: PathUri,
+        source: ExecCommandSource,
+        name: String,
+        path: PathBuf,
+    ) -> Self {
+        let cmd = command.join(" ");
+        Self::Shell {
+            command,
+            cwd,
+            source,
+            parsed_cmd: vec![ParsedCommand::Read { cmd, name, path }],
+            plugin_attribution: None,
+        }
+    }
+
     pub fn unified_exec(
         command: &[String],
         cwd: PathUri,