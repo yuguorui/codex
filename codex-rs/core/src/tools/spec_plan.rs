yuguorui/codex-plus

core: add Claude Code compatible file tools

codex-rs/core/src/tools/spec_plan.rs
@@ -12,6 +12,8 @@ use crate::tools::handlers::CurrentTimeHandler;
 use crate::tools::handlers::DynamicToolHandler;
 use crate::tools::handlers::ExecCommandHandler;
 use crate::tools::handlers::ExecCommandHandlerOptions;
+use crate::tools::handlers::FileEditHandler;
+use crate::tools::handlers::FileReadHandler;
 use crate::tools::handlers::GetContextRemainingHandler;
 use crate::tools::handlers::HashlineHandler;
 use crate::tools::handlers::ListAvailablePluginsToInstallHandler;
@@ -34,6 +36,8 @@ use crate::tools::handlers::WriteStdinHandler;
 use crate::tools::handlers::agent_jobs::ReportAgentJobResultHandler;
 use crate::tools::handlers::agent_jobs::SpawnAgentsOnCsvHandler;
 use crate::tools::handlers::extension_tools::ExtensionToolAdapter;
+use crate::tools::handlers::file_edit_spec::FileEditToolOptions;
+use crate::tools::handlers::file_read_spec::FileReadToolOptions;
 use crate::tools::handlers::hashline_spec::HashlineToolOptions;
 use crate::tools::handlers::multi_agents::CloseAgentHandler;
 use crate::tools::handlers::multi_agents::ResumeAgentHandler;
@@ -65,6 +69,7 @@ use codex_login::AuthManager;
 use codex_protocol::config_types::WebSearchMode;
 use codex_protocol::dynamic_tools::DynamicToolNamespaceTool;
 use codex_protocol::dynamic_tools::DynamicToolSpec;
+use codex_protocol::openai_models::ApplyPatchToolType;
 use codex_protocol::openai_models::ConfigShellToolType;
 use codex_protocol::openai_models::InputModality;
 use codex_protocol::openai_models::ToolMode;
@@ -758,13 +763,29 @@ fn add_core_utility_tools(context: &CoreToolPlanContext<'_>, planned_tools: &mut
         ));
     }
 
-    if environment_mode.has_environment() && turn_context.model_info.apply_patch_tool_type.is_some()
+    if environment_mode.has_environment()
+        && let Some(apply_patch_tool_type) = turn_context.model_info.apply_patch_tool_type.as_ref()
     {
-        let include_environment_id = matches!(environment_mode, ToolEnvironmentMode::Multiple);
-        planned_tools.add(ApplyPatchHandler::new(include_environment_id));
-        planned_tools.add(HashlineHandler::new(HashlineToolOptions {
-            include_environment_id,
-        }));
+        match apply_patch_tool_type {
+            ApplyPatchToolType::Freeform => {
+                let include_environment_id =
+                    matches!(environment_mode, ToolEnvironmentMode::Multiple);
+                planned_tools.add(ApplyPatchHandler::new(include_environment_id));
+                planned_tools.add(HashlineHandler::new(HashlineToolOptions {
+                    include_environment_id,
+                }));
+            }
+            ApplyPatchToolType::ClaudeCode => {
+                let include_environment_id =
+                    matches!(environment_mode, ToolEnvironmentMode::Multiple);
+                planned_tools.add(FileReadHandler::new(FileReadToolOptions {
+                    include_environment_id,
+                }));
+                planned_tools.add(FileEditHandler::new(FileEditToolOptions {
+                    include_environment_id,
+                }));
+            }
+        }
     }
 
     if turn_context

yuguorui/codex

codex-rs/core/src/tools/spec_plan.rs
@@ -14,6 +14,8 @@ use crate::tools::handlers::CurrentTimeHandler;
 use crate::tools::handlers::DynamicToolHandler;
 use crate::tools::handlers::ExecCommandHandler;
 use crate::tools::handlers::ExecCommandHandlerOptions;
+use crate::tools::handlers::FileEditHandler;
+use crate::tools::handlers::FileReadHandler;
 use crate::tools::handlers::GetContextRemainingHandler;
 use crate::tools::handlers::HashlineHandler;
 use crate::tools::handlers::ListAvailablePluginsToInstallHandler;
@@ -33,6 +35,8 @@ use crate::tools::handlers::ViewImageHandler;
 use crate::tools::handlers::WaitForEnvironmentHandler;
 use crate::tools::handlers::WriteStdinHandler;
 use crate::tools::handlers::extension_tools::ExtensionToolAdapter;
+use crate::tools::handlers::file_edit_spec::FileEditToolOptions;
+use crate::tools::handlers::file_read_spec::FileReadToolOptions;
 use crate::tools::handlers::hashline_spec::HashlineToolOptions;
 use crate::tools::handlers::multi_agents::CloseAgentHandler;
 use crate::tools::handlers::multi_agents::ResumeAgentHandler;
@@ -72,6 +76,7 @@ use codex_protocol::dynamic_tools::DynamicToolSpec;
 use codex_protocol::error::CodexErrorDetails;
 use codex_protocol::error::Result as CodexResult;
 use codex_protocol::models::PermissionProfile;
+use codex_protocol::openai_models::ApplyPatchToolType;
 use codex_protocol::openai_models::ConfigShellToolType;
 use codex_protocol::openai_models::InputModality;
 use codex_protocol::openai_models::ToolMode;
@@ -1086,13 +1091,29 @@ fn add_core_utility_tools(context: &CoreToolPlanContext<'_>, registry: &mut Tool
         ));
     }
 
-    if environment_mode.has_environment() && turn_context.model_info.apply_patch_tool_type.is_some()
+    if environment_mode.has_environment()
+        && let Some(apply_patch_tool_type) = turn_context.model_info.apply_patch_tool_type.as_ref()
     {
-        let include_environment_id = matches!(environment_mode, ToolEnvironmentMode::Multiple);
-        registry.add(ApplyPatchHandler::new(include_environment_id));
-        registry.add(HashlineHandler::new(HashlineToolOptions {
-            include_environment_id,
-        }));
+        match apply_patch_tool_type {
+            ApplyPatchToolType::Freeform => {
+                let include_environment_id =
+                    matches!(environment_mode, ToolEnvironmentMode::Multiple);
+                registry.add(ApplyPatchHandler::new(include_environment_id));
+                registry.add(HashlineHandler::new(HashlineToolOptions {
+                    include_environment_id,
+                }));
+            }
+            ApplyPatchToolType::ClaudeCode => {
+                let include_environment_id =
+                    matches!(environment_mode, ToolEnvironmentMode::Multiple);
+                registry.add(FileReadHandler::new(FileReadToolOptions {
+                    include_environment_id,
+                }));
+                registry.add(FileEditHandler::new(FileEditToolOptions {
+                    include_environment_id,
+                }));
+            }
+        }
     }
 
     if turn_context