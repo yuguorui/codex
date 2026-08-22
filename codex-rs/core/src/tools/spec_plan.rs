yuguorui/codex-plus

codex-cli: add hashline tool
Add a new hashline tool that enables content-addressed line-level edits
using 2-character xxHash anchors. This provides a more robust alternative
to context-based patching by targeting exact line content rather than
surrounding text.

- New handlers/hashline.rs: implements read, edit, insert, delete, and
  verify actions with fuzzy relocate fallback for stale anchors
- New handlers/hashline_spec.rs: defines the tool schema with parameter
  validation for action, path, anchor, content, context, and before
- Fuzzy matching relocates to nearby lines when hash anchors don't match
  exactly, reducing false rejections from minor whitespace drift
- Generates file change events and diff output compatible with existing
  tool infrastructure
- Comprehensive test coverage for all actions and edge cases

codex-rs/core/src/tools/spec_plan.rs
@@ -9,6 +9,7 @@ use crate::tools::handlers::DynamicToolHandler;
 use crate::tools::handlers::ExecCommandHandler;
 use crate::tools::handlers::ExecCommandHandlerOptions;
 use crate::tools::handlers::GetGoalHandler;
+use crate::tools::handlers::HashlineHandler;
 use crate::tools::handlers::ListAvailablePluginsToInstallHandler;
 use crate::tools::handlers::ListMcpResourceTemplatesHandler;
 use crate::tools::handlers::ListMcpResourcesHandler;
@@ -28,6 +29,7 @@ use crate::tools::handlers::WriteStdinHandler;
 use crate::tools::handlers::agent_jobs::ReportAgentJobResultHandler;
 use crate::tools::handlers::agent_jobs::SpawnAgentsOnCsvHandler;
 use crate::tools::handlers::extension_tools::ExtensionToolAdapter;
+use crate::tools::handlers::hashline_spec::HashlineToolOptions;
 use crate::tools::handlers::multi_agents::CloseAgentHandler;
 use crate::tools::handlers::multi_agents::ResumeAgentHandler;
 use crate::tools::handlers::multi_agents::SendInputHandler;
@@ -626,6 +628,9 @@ fn add_core_utility_tools(context: &CoreToolPlanContext<'_>, planned_tools: &mut
     {
         let include_environment_id = matches!(environment_mode, ToolEnvironmentMode::Multiple);
         planned_tools.add(ApplyPatchHandler::new(include_environment_id));
+        planned_tools.add(HashlineHandler::new(HashlineToolOptions {
+            include_environment_id,
+        }));
     }
 
     if turn_context

yuguorui/codex

codex-rs/core/src/tools/spec_plan.rs
@@ -15,6 +15,7 @@ use crate::tools::handlers::DynamicToolHandler;
 use crate::tools::handlers::ExecCommandHandler;
 use crate::tools::handlers::ExecCommandHandlerOptions;
 use crate::tools::handlers::GetContextRemainingHandler;
+use crate::tools::handlers::HashlineHandler;
 use crate::tools::handlers::ListAvailablePluginsToInstallHandler;
 use crate::tools::handlers::ListMcpResourceTemplatesHandler;
 use crate::tools::handlers::ListMcpResourcesHandler;
@@ -32,6 +33,7 @@ use crate::tools::handlers::ViewImageHandler;
 use crate::tools::handlers::WaitForEnvironmentHandler;
 use crate::tools::handlers::WriteStdinHandler;
 use crate::tools::handlers::extension_tools::ExtensionToolAdapter;
+use crate::tools::handlers::hashline_spec::HashlineToolOptions;
 use crate::tools::handlers::multi_agents::CloseAgentHandler;
 use crate::tools::handlers::multi_agents::ResumeAgentHandler;
 use crate::tools::handlers::multi_agents::SendInputHandler;
@@ -1088,6 +1090,9 @@ fn add_core_utility_tools(context: &CoreToolPlanContext<'_>, registry: &mut Tool
     {
         let include_environment_id = matches!(environment_mode, ToolEnvironmentMode::Multiple);
         registry.add(ApplyPatchHandler::new(include_environment_id));
+        registry.add(HashlineHandler::new(HashlineToolOptions {
+            include_environment_id,
+        }));
     }
 
     if turn_context